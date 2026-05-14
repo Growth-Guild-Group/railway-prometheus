e814331be080195dd2f3c656130bd66cebddbec0
Jason Dinh <83333158+DinhJDev@users.noreply.github.com>
2026-05-14 18:19:24 -0500

    Fix formatting issues in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 set -eu
-  : "${LUCIDSERUM_METRICS_TOKEN:?LUCIDSERUM_METRICS_TOKEN must be set in Railway}"
-  umask 077
-  printf '%s' "$LUCIDSERUM_METRICS_TOKEN" > /tmp/lucidserum-metrics-token
-  exec /bin/prometheus "$@"
+: "${LUCIDSERUM_METRICS_TOKEN:?LUCIDSERUM_METRICS_TOKEN must be set in Railway}"
+umask 077
+printf '%s' "$LUCIDSERUM_METRICS_TOKEN" > /tmp/lucidserum-metrics-token
+exec /bin/prometheus "$@"
